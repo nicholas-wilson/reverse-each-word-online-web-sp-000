32c818b5a1b338bbf5e267c5af7d834a842a63e7
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-19 00:40:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   sentence = ""
   words.collect do |word|
     word = word.reverse
-    puts word
+    puts words[words.length - 1]
     if (words[words.length - 1] == word)
       sentence << "#{word}"
     else
